d1ec80304ff2aaf74a139e44b0166e40de143a55
Arkadiusz Staśko <Arkadiusz.Stasko@microsoft.wsei.edu.pl>
2022-03-10 11:29:01 +0100

    add structure of Ulamek class without full implementation and tests

---
+++ ObjectiveProgramming/Ulamek.cs
@@ -0,0 +1,48 @@
+﻿
+namespace ObjectiveProgramming
+{
+    public class Ulamek : IEquatable<Ulamek>, IComparable<Ulamek>
+    {
+        private int licznik;
+        private int mianownik;
+
+        public Ulamek() { }
+        public Ulamek(int number1, int number2)
+        {
+            licznik = number1;
+            mianownik = number2;
+        }
+
+        public Ulamek(Ulamek prev)
+        {
+            licznik = prev.licznik;
+            mianownik = prev.mianownik;
+        }
+
+        public override string ToString()
+        {
+            return $"{licznik}/{mianownik}";
+        }
+
+        public static Ulamek operator +(Ulamek a, Ulamek b)
+        {
+            return new Ulamek(a.licznik + b.licznik, a.mianownik);
+        }
+
+        public static Ulamek operator -(Ulamek a, Ulamek b)
+        {
+            return new Ulamek(a.licznik - b.licznik, a.mianownik);
+        }
+
+        public static Ulamek operator *(Ulamek a, Ulamek b)
+        {
+            return new Ulamek(a.licznik * b.licznik, a.mianownik * b.mianownik);
+        }
+         
+        public static Ulamek operator /(Ulamek a, Ulamek b)
+        {
+            return new Ulamek(a.licznik * b.mianownik, a.mianownik * b.licznik);
+        }
+
+    }
+}
---
+++ ObjectiveProgramming/View.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectiveProgramming
+{
+    public class View
+    {
+        public void PrintMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+    }
+}
